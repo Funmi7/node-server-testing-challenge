a78201d5f5f9e6f08b99509cd57dd1b5f0711e33
Funmi7 <talabifunmilayo@gmail.com>
2019-11-14 16:41:59 +0100

    Added more tests to test api is working

--- server.spec.js
+++ server.spec.js
@@ -8,4 +8,28 @@ describe('server', () => {
     })
   })
 
+  test('should return 200 OK', async () => {
+    const response = await request(server).get('/')
+    expect(response.status).toBe(200)
+  })
+
+  test('should return 200 OK with Es6 promise', () => {
+    return request(server).get('/')
+    .then(response => {
+      expect(response.status).toBe(200)
+    })
+  })
+
+  test('with superest syntax', () => {
+    return request(server).get('/')
+    .expect(200)
+    .expect({ api: 'up'})
+    .expect('content-Length', "12")
+    .expect('content-Type', /json/)
+  })
+
+  test('returns the right response body', async () => {
+    const response = await request(server).get('/')
+    expect(response.body).toEqual({ api: 'up' });
+  })
 })
